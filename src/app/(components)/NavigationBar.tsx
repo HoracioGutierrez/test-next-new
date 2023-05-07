8b8fbbb8be91bfc1e6d46a8b7877d860c23e1607
HoracioGutierrez <horacio.estevez@gmail.com>
2023-05-07 18:30:48 -0300

    Fix navbar conditional

--- src/app/(components)/NavigationBar.tsx
+++ src/app/(components)/NavigationBar.tsx
@@ -6,10 +6,10 @@ type Props = {
 
 export default function NavigationBar({ isPage }: Props) {
   return (
-    <nav className={`grid grid-cols-${ isPage ? "3" : "2" } gap-4`}>
-      {isPage && <Link className={`text-gray-${isPage ? "400" : "600"} hover:text-gray-${isPage ? "200" : "400"}`} href="/">home</Link>}
-      <Link className={`text-gray-${isPage ? "400" : "600"} hover:text-gray-${isPage ? "200" : "400"}`} href="/stars">starts</Link>
-      <Link className={`text-gray-${isPage ? "400" : "600"} hover:text-gray-${isPage ? "200" : "400"}`} href="/about">about</Link>
+    <nav className={`flex gap-4`}>
+      {isPage && <Link className="text-gray-400 hover:text-gray-200" href="/">home</Link>}
+      <Link className="text-gray-400 hover:text-gray-200" href="/stars">starts</Link>
+      <Link className="text-gray-400 hover:text-gray-200" href="/about">about</Link>
     </nav>
   )
 }
