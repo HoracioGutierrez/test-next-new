f01e0c2a07db7e3f42435d4b1780c57a0d65176e
HoracioGutierrez <horacio.estevez@gmail.com>
2023-05-07 20:23:05 -0300

    Fixed social links

--- src/app/(pages)/@content/about/page.tsx
+++ src/app/(pages)/@content/about/page.tsx
@@ -5,9 +5,9 @@ export default function AboutContent({}: Props) {
   return (
     <div className="h-full">
       <nav className="flex justify-center gap-4">
-        <a href="http://instagram.com"><InstagramLogoIcon width={30} height={30}/></a>
-        <a href="http://linkedin.com"><LinkedInLogoIcon width={30} height={30}/></a>
-        <a href="http://github.com"><GitHubLogoIcon width={30} height={30}/></a>
+        <a href="https://www.instagram.com/horagutierrez"><InstagramLogoIcon width={30} height={30}/></a>
+        <a href="https://www.linkedin.com/in/horacioegutierrez/"><LinkedInLogoIcon width={30} height={30}/></a>
+        <a href="https://github.com/HoracioGutierrez"><GitHubLogoIcon width={30} height={30}/></a>
       </nav>
     </div>
   )
