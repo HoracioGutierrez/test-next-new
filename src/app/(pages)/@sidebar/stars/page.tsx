72e65066181aed07c6b0ffc61fb961e585ab3bd8
HoracioGutierrez <horacio.estevez@gmail.com>
2023-05-07 19:06:27 -0300

    Add content instead of lorem ipsum

--- src/app/(pages)/@sidebar/stars/page.tsx
+++ src/app/(pages)/@sidebar/stars/page.tsx
@@ -7,8 +7,10 @@ export default function StarsSidebar({ }: Props) {
         <h2 className="text-2xl font-semibold">Stars</h2>
       </header>
       <section>
-        <p className="pb-1">Stars are hot.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, harum neque. Expedita veniam fugiat adipisci. Tempora tenetur beatae consequatur culpa dolor, necessitatibus at pariatur, consectetur, ipsum sequi voluptatum voluptatibus vero!</p>
+        <p className="pb-3 text-sm text-gray-400 italic">"Stars are hot"</p>
+        <p className="pb-3">En esta pagina queda demostrado como un componente de servidor puede traer desde la base de datos directamente informacion colocalizando en el mismo archivo la parte que lo va a mostrar.</p>
+        <p className="pb-3">En una proxima iteracion, se va a agregar la parte del loading y streaming por lazi loading.</p>
+        <p>Mas adelante para terminar se va a agregar el detalle de cada recurso junto con la posibilidad de dar de alta y baja</p>
       </section>
     </>
   )
