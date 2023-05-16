54a37a693f88f31c58fbc02fccb40b7475e2f86f
HoracioGutierrez <horacio.estevez@gmail.com>
2023-05-16 15:05:46 -0300

    Add avatar picture and refactor landing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,24 @@
 import { InstagramLogoIcon, LinkedInLogoIcon, GitHubLogoIcon } from "@radix-ui/react-icons"
 import NavigationBar from "../(components)/NavigationBar"
+import Image from 'next/image';
+import Avatar from "../(assets)/avatar.png"
 
- 
+
 export default function Home() {
   return (
     <>
+      <Image src={Avatar} alt="Horacio Gutierrez Avatar" width={200} height={200} className="rounded-full mx-auto" />
       <h1 className="text-4xl font-bold text-white mb-12">Bienvenido!</h1>
       <div className="max-w-2xl mx-auto text-center">
-        <p className="text-gray-300 mb-8">Esta es una pagina en construccion. Supuestamente iba a terminar un portfolio funcional en Next.js peeeero justo sacaron una nueva version asi que nada, Jr de nuevo.</p>
-        <p className="text-gray-300 mb-2" >Actualmente podrian navegar solo a traves de estas dos paginas</p>
+        <p className="text-gray-300 mb-8">Esta es una pagina en construccion. Supuestamente iba a terminar un portfolio funcional en Next.js peeeero justo sacaron una nueva version asi que nada, Jr de nuevo. Actualmente podrian navegar solo a traves de estas tres paginas</p>
         <div className="flex justify-center mb-8">
-          <NavigationBar/>
+          <NavigationBar />
         </div>
         <p className="text-gray-300 mb-2">Van a tener que tenerme paciencia porque momentaneamente sigo probando cosas nuevas como</p>
-        <ul className="text-yellow-200 mb-8 list-disc w-fit m-auto">
-          <li>rutas paralelas</li>
-          <li>rutas agrupatas</li>
-          <li>opt-out layouts</li>
+        <ul className="text-yellow-200 mb-8 w-fit m-auto flex">
+          <li>rutas paralelas<span className="text-gray-300 mx-2">|</span></li>
+          <li>rutas agrupatas<span className="text-gray-300 mx-2">|</span></li>
+          <li>opt-out layouts<span className="text-gray-300 mx-2">|</span></li>
           <li>next actions</li>
         </ul>
         <p className="text-gray-300 mb-8">Si necesitas contacto directo conmigo por algun motivo te dejo ya estos links</p>
